c1b37ba871225f2e59345c2819af34d4355c6893
Shoaib <shoaibrochi9@gmail.com>
2024-01-30 12:57:19 +0600

    add fetch api an show it in console

--- js/script.js
+++ js/script.js
@@ -2,3 +2,20 @@ let searchBtn = document.querySelector("#searchBtn");
 let searchUser = document.querySelector("#searchUser");
 let profile = document.querySelector("#profile");
 
+searchBtn.addEventListener("click", function handleClick(e) {
+    let userText = searchUser.value;
+    // if (userText!=""){
+    //     alert(userText);
+    // }
+    // else(alert("Empty"));
+    fetch(`https://api.github.com/users/${userText}`)
+        .then(fetchedData => fetchedData.json())
+        .then(fetchedData => {
+            if (fetchedData.message == "Not Found") {
+                alert("User Not found")
+            }
+            else {
+                alert(JSON.stringify(fetchedData))
+            }
+        })
+});
